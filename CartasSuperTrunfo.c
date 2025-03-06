2d66a522c5b70a48ff59e622c9af344ccc952cae
bruprogramadora <sbruna24@gmail.com>
2025-03-06 01:58:39 +0000

    echo "# super-trunfo-paises" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/bruprogramadora/super-trunfo-paises.git git push -u origin main

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+// Desafio Super Trunfo -- Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
